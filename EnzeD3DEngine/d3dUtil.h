17d91621d5775e3a9c6c477727b8aa7d233da41b
enzeDamon <92028941+acse-ep20@users.noreply.github.com>
2023-03-02 16:18:33 +0800

    unresolved iid device problem solved

--- EnzeD3DEngine/d3dUtil.h
+++ EnzeD3DEngine/d3dUtil.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "stdafx.h"
-
+#include "DXSampleHelper.h"
 class d3dUtil
 {
     public:
         static UINT CalcConstantBufferByteSize(UINT byteSize){
             return (byteSize + 255) & ~255;
         }
+        static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
+            ID3D12Device* device,
+            ID3D12GraphicsCommandList* cmdList,
+            const void* initData,
+            UINT64 byteSize,
+            Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer);
 
 };
